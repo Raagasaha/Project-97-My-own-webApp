d2438272d4e3617ed0d97ffe2da2bd3d6d2b94e4
Raagasaha <122801791+Raagasaha@users.noreply.github.com>
2023-08-31 13:30:06 -0400

    Add files via upload

---
+++ 2ndPage.js
@@ -0,0 +1,18 @@
+user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML="Hello " + user_name + "!" 
+
+function tylenolPage() {
+    location.replace("tylenol.html");
+}
+
+function zyrtecPage() {
+    location.replace("zyrtec.html");
+}
+
+function advilPage() {
+    location.replace("advil.html");
+}
+
+function aspirinPage() {
+    location.replace("aspirin.html");
+}
